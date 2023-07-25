bf62ae81575ba34f9cc7e3aa4bd2c4dcdcae1f4e
Paul Smith <paul@blinkylights.org>
2023-07-25 11:14:07 +0200

    Example script

--- examples/duck.js
+++ examples/duck.js
@@ -3,9 +3,7 @@ import axiosProfiler from '../src/index.js'
 
 const profilerAxios = axiosProfiler(axios)
 
-const getTheMoff = () => {
-  return profilerAxios.get('https://duckduckgo.com/?q=grand+moff+tarkin&format=json&pretty=1')
-}
+const getTheMoff = () => profilerAxios.get('https://duckduckgo.com/?q=grand+moff+tarkin&format=json&pretty=1')
 
 getTheMoff().then(response => {
   console.log(JSON.stringify(response.headers['x-axios-duration'], null, 2))
